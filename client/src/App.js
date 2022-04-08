f4492a8bbadf37cd27e6995609c7ba74fd368e2e
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-08 22:12:11 +0800

    first

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    const url = "http://localhost:3001";
+    axios.get(url).then((res) => console.log(res.data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
